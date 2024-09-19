583db5daf2f86a771897c1b4309ea9d309bc897e
inesgassiebayle <110676544+inesgassiebayle@users.noreply.github.com>
2024-09-19 17:11:27 -0300

    Add files via upload

---
+++ src/main/java/com/ingsis/jcli/printscript_service/PrintscriptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.printscript_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintscriptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintscriptServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsis/jcli/printscript_service/PrintscriptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.printscript_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintscriptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
