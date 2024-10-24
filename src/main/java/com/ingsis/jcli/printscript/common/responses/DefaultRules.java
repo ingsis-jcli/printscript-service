5cb07d3f0208cac8b17532e693802c5957f78660
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-23 22:38:41 -0300

    initial hello for the redis

--- src/main/java/com/ingsis/jcli/printscript/common/responses/DefaultRules.java
+++ src/main/java/com/ingsis/jcli/printscript/common/responses/DefaultRules.java
@@ -1,7 +1,8 @@
 package com.ingsis.jcli.printscript.common.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ingsis.jcli.printscript.common.Generated;
 import java.util.List;
 
 @Generated
-public record DefaultRules(List<DefaultRule> rules) {}
+public record DefaultRules(@JsonProperty("rules") List<DefaultRule> rules) {}
