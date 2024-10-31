ddb7f25f15b2b490afbf6f1c63dae32a6702cb1e
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-31 15:19:03 -0300

    todo's

--- src/main/java/com/ingsis/jcli/printscript/auth0/Auth0Configuration.java
+++ src/main/java/com/ingsis/jcli/printscript/auth0/Auth0Configuration.java
@@ -5,7 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.*;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
