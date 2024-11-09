b49e4e23eec8889787f6e5b88c756b669bfac868
luzlaura <llaura@mail.austral.edu.ar>
2024-11-08 21:03:52 -0300

    Added interceptor FeignConfig

--- src/main/java/com/ingsis/jcli/printscript/config/FeignConfig.java
+++ src/main/java/com/ingsis/jcli/printscript/config/FeignConfig.java
@@ -1,11 +1,29 @@
 package com.ingsis.jcli.printscript.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+  private static final String CORRELATION_ID_KEY = "correlation-id";
+
+  @Bean
+  public RequestInterceptor correlationIdRequestInterceptor() {
+    return new RequestInterceptor() {
+      @Override
+      public void apply(RequestTemplate template) {
+        String correlationId = MDC.get(CORRELATION_ID_KEY);
+        if (correlationId != null) {
+          template.header("X-Correlation-Id", correlationId);
+        }
+      }
+    };
+  }
 
   @Bean
   Logger.Level feignLoggerLevel() {
